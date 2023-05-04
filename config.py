0c767cacedc644f41097b2e157a8e2a673622ccb
Анна Беляева <ann-z@bk.ru>
2023-05-05 00:41:34 +0300

    work4

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-comunity_token = 'vk1.a.U6EYPcNST2PLw6CRHRj161o4U_pwDCVY1XbOTxKRR3Qn1C_4aeW5EE2kpuUCihwiOHuC1g1Vs43SSVySRK1SFVFWijnVx7PekT83686IujrrkqmW1jqmfmjIqSST_pgR7Kvwy6rAdm2epODgE8yDaZGULJg26zDO70heclJOxFy7vrFpKymIG2-2FqytJwt5PaH3nPq7gmHsRYop23EOdA'
-acces_token = 'vk1.a.5bkFsImcOzz-fJbfKLOpoRlZrKSkC6u8PI2w9RQSGdN9BOwLfht6PjHnHcGgwkW3cz_svFnCsYshqptSrpHiVUZAakF1CcX7L7BhzBdCrBPWbtedITJKsEjtyeFkXs--AePh2pZ27F4kNoP31Ms9WRMD9IxJhwa9Q6w9OvPf65oLgN2rNBqAvfUQTS1C2aRD'
+comunity_token = ''
+acces_token = ''
 
 
 user_bd = 'postgres'
